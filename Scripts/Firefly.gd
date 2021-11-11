0d40022e06669d1012d5513e17d47ceb310991e0
mgrigajtis <matthew.grigajtis@gmail.com>
2021-11-11 13:45:16 -0500

    Adds Firefly sprite

---
+++ Scripts/Firefly.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+onready var globals = Globals
+var ySpeed = 0
+var soundEffect = preload("res://SoundEffects/score.ogg")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#$Area2D/AudioStreamPlayer.stream = soundEffect
+	get_node("Area2D/AnimationPlayer").play ("Fly", -1, 1.0, true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	position.y += (ySpeed * globals.Difficulty)
+
+func _on_Area2D_area_entered(area):
+	if "areaType" in area:
+		if area.areaType == "DestroyLine":
+			queue_free()
+			
+		if area.areaType == "Spider":
+			#if !get_tree().get_root().get_node("Game/EffectsPlayer").playing:
+			#	get_tree().get_root().get_node("Game/EffectsPlayer").play()
+				
+			globals.Score += 75
+			get_tree().get_root().get_node("Game/UI/Score").text = str(globals.Score)
+			
+			queue_free()
